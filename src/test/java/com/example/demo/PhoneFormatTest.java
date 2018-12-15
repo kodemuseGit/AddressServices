3ba4127eebb9e4f9fa814273a4186d39320a6a5a
naveen475215 <naveen.47521505@gmail.com>
2018-12-15 06:08:08 -0500

    Added new test file

---
+++ src/test/java/com/example/demo/PhoneFormatTest.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PhoneFormatTest {
+
+	@Test
+	public void validLength() {
+		String phone = "1234567890";
+	 	assertEquals(true, phone.matches("[0-9]{1,10}"));
+	}
+	
+	@Test
+	public void inValidLength() {
+		String phone = "12345678901";
+	 	assertEquals(true, phone.matches("[0-9]{1,10}"));
+	}
+	
+	@Test
+	public void validFormat() {
+		String phone = "123456789";
+	 	assertEquals(true, phone.matches("[0-9]{1,10}"));
+	}
+	
+	@Test
+	public void inValidFormat() {
+		String phone = "12345678a";
+	 	assertEquals(true, phone.matches("[0-9]{1,10}"));
+	}
+}
